6ec21f93fdbe44815a32253b28226f6208be7205
Brice Tchijouk <63953094+Brissok@users.noreply.github.com>
2021-06-10 17:41:30 +0200

    déploiement serveur

---
+++ Backend/app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const userRoutes = require('./routes/user');
+//const sauceRoutes = require('./routes/sauce');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+//app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
