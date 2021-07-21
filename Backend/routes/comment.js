22bf92f730d1cdfd8916c489f111ba3606771b8b
Brice Tchijouk <63953094+Brissok@users.noreply.github.com>
2021-07-21 19:02:12 +0200

    script bootstrap ok

--- Backend/routes/comment.js
+++ Backend/routes/comment.js
@@ -4,10 +4,7 @@ const router = express.Router();
 const commentCtrl = require('../controllers/comment');
 const auth = require('../middleware/auth');
 
-//router.get('/', commentCtrl.getComments);
 router.post('/', commentCtrl.createComment);
-//router.put('/:id', commentCtrl.modifyComment);
 router.delete('/:id', commentCtrl.deleteComment);
-
 
 module.exports = router;
